df634dc0cbb19adf24fcf45c536e3cda7cab335d
chantelle.burton <chantelle.burton@.metoffice.gov.uk>
2023-10-27 16:28:24 +0100

    Fixing some bugs in the uncertainty code

--- CodeForPlots/Global_Uncertainty_Analysis.py
+++ CodeForPlots/Global_Uncertainty_Analysis.py
@@ -1,14 +1,6 @@
-### FINAL Working global code ####
-
-
 import os
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
 import seaborn as sns
-import iris
-from iris import plot
 import cf_units
 import cftime
 import cartopy
@@ -22,166 +14,73 @@ from scipy import stats
 from scipy.signal import periodogram
 from scipy.stats import pearsonr
 import random
+warnings.filterwarnings('ignore')
 
-SEED = int.from_bytes("They're taking the hobbits to Isengard!".encode('utf-8'), byteorder='big')
-
-def constrain_time(cube, date1, date2):
-    if not any([coord.long_name == 'year' for coord in cube.coords()]):
-        iris.coord_categorisation.add_year(cube, 'time', name='year')
-    return cube.extract(iris.Constraint(time=lambda cell: date1 <= cell.point.year <= date2))
-
-def to_timeseries(cube):
-    coords = ('longitude', 'latitude')
-    for coord in coords:
-        if not cube.coord(coord).has_bounds():
-            cube.coord(coord).guess_bounds()
-    weights = iris.analysis.cartography.area_weights(cube)
-    return cube.collapsed(coords, iris.analysis.SUM, weights = weights)/10**12
-
-def to_monthly_mean(cube):
-    if not any([coord.long_name == 'month' for coord in cube.coords()]):
-        iris.coord_categorisation.add_month(cube, 'time', name='month')
-    return cube.aggregated_by('month', iris.analysis.MEAN)
-
-def pixel_mean(cube):
-    return cube.collapsed(('time'), iris.analysis.MEAN)
-
-def to_monthly_max(cube):
-    if not any([coord.long_name == 'month' for coord in cube.coords()]):
-        iris.coord_categorisation.add_month(cube, 'time', name='month')
-    return cube.aggregated_by('month', iris.analysis.MAX)
-
-def to_annual(cube):
-    if not any([coord.long_name == 'year' for coord in cube.coords()]):
-        iris.coord_categorisation.add_year(cube, 'time', name='year')
-    return cube.aggregated_by(['year'], iris.analysis.SUM)
-
-def add_year(cube):
-    if not any([coord.long_name == 'year' for coord in cube.coords()]):
-        iris.coord_categorisation.add_year(cube, 'time', name='year')
-    return cube
-
-def to_df(cube, var_name='month', val_name='Burned Area (in Mha)'):
-    cube_data = cube.data.compressed().reshape(cube.shape[0], -1)
-    cube_df = pd.DataFrame(data=cube_data).transpose().melt()
-    cube_df.rename(columns = {'variable':var_name, 'value':val_name}, inplace = True)
-    return cube_df
-
-def to_percentiles(cube, percentiles):
-    coords = ('longitude', 'latitude')
-    for coord in coords:
-        if not cube.coord(coord).has_bounds():
-            cube.coord(coord).guess_bounds()
-    return cube.collapsed(coords, iris.analysis.PERCENTILE, percent=percentiles)
-
-def from_perc_to_mha(cube):
-    weights = iris.analysis.cartography.area_weights(cube)
-    return iris.analysis.maths.multiply(cube, weights)/10**12
-
-def mask_below_percentile(cube, p=0.99):
-    pXX = cube.collapsed(('longitude', 'latitude', 'time'), iris.analysis.PERCENTILE, percent=p)
-    mask = xr.where(cube.data < pXX.data, True, False)
-    return iris.util.mask_cube(cube, mask)
-
-def count_per_time(masked_cube):
-    counts = masked_cube.collapsed(('longitude', 'latitude'), iris.analysis.COUNT, function=lambda values: values > 0)
-    return counts
-
-def to_Z_score(cube):
-    anomaly = cube-cube.collapsed('time', iris.analysis.MEAN)
-    return anomaly/np.std(cube.data)
-
-def df_to_annual(df):
-    # 1. Convert the index to year to_period('Y'), which turns it into a pandas.PeriodIndex.
-    # 2. Get the year as int (.year, seaborn doesn't know how to handle a pandas.PeriodIndex)
-    # 3. Group by year .groupby()
-    # 4. Take the sum of each group .sum()
-    # 5. NaNs are treated as 0s, and a series of all NaNs would equal to 0. With min_count=1, it equals NaN if there are less than min_count (1) real values in the series.;
-    # The difference between 0 and NaN is important for plots (NaNs) aren't shown and for statistics e.g., mean.
-    return df.groupby(df.index.to_period('Y').year).sum(min_count=1)
-
-def prepare_for_facetgrid(df):
-    return df.melt(ignore_index=False).reset_index().rename(columns={'value': 'BA'})
-
-def drop_model(df, modelname):
-    return df.drop(modelname, axis=1, level=1)
-
-def drop_models(df, modelnames):
-    for modelname in modelnames:
-        df = drop_model(df, modelname)
-    return df
-
-def select_model(df, modelname):
-    return df.loc[:, (slice(None), modelname)]
-
-def select_models(df, modelnames):
-    return df.loc[:, (slice(None), modelnames)]
-
-def select_region(df, regionname):
-    return df.loc[:, (regionname, slice(None))]
-
-def to_anomaly(df):
-    return df - df.mean()
-
-def to_relative_anomaly(df):
-    return (df - df.mean())/df.mean()
 
 def df_constrain_time(df, start, end):
     return df.loc[str(start):str(end)]
-
-def df_to_global(df):
-    if 'Observation' in df.columns.names:
-        level = 'Observation'
-    elif 'Model' in df.columns.names:
-        level = 'Model'
-    else:
-        raise ValueError("Observation or Model should be in the column level names (df.column.names)")
-    return df.groupby(level=level, axis=1).sum(min_count=1)
-
-
-
-
-
-
-
-
-#######Get weights#####
+def select_region(df, regionname):
+    return df.loc[:, (regionname, slice(None))]
+SEED = int.from_bytes("They're taking the hobbits to Isengard!".encode('utf-8'), byteorder='big')
 
 
 obsclim_df = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_obsclim_df.pkl')
 counterclim_df = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_counterclim_df.pkl')
 model_weights = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/NME3_Weights.pkl')
 
-##Set up obsclim relative anomaly
+
+
+#######################################Get weights########################################################
+
+    
+def log_diff(obs_DF, model_DF):
+    return np.mean(np.abs(np.log(model_DF+1.0) - np.log(obs_DF[:,0]+1.0)))
+
+#Set up obsclim relative anomaly
 obsclim_global = obsclim_df.groupby('Model', axis=1).sum()
 obsclim = df_constrain_time(obsclim_global, 2003, 2019)
 model_DF = (obsclim-obsclim.mean(axis=0))/obsclim.mean(axis=0)
 
-##Set up obs relative anomaly
-    
-# Make GFED5 NME
-obs = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_obs_df.pkl')
-obs_global = obs_df.groupby('Observation', axis=1).sum()
-obs_constrainedtime = df_constrain_time(obs_global, 2003, 2019)
-obs_GFED = (obs_constrainedtime['GFED5'])
-obs_DF_GFED = (obs_GFED-obs_GFED.mean(axis=0))/obs_GFED.mean(axis=0)
+#Set up obs relative anomaly
 
-GFED5weights = (np.abs(np.log(model_DF+1.0+(1/204))).subtract(np.log(obs_DF_GFED+1.0+(1/204)), axis=0))# Model vs Obs anomaly
-GFED5weights[:] = GFED5weights.mean(axis=0)
+# Make temporal NME for GFED5 first
+obs = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_obs_df.pkl')#2 obs, CCI5.1 & MCD64
+obs_time = df_constrain_time(obs, 2003, 2019)
+obs_global = obs_time.groupby(by='Observation', axis=1).sum(min_count=1) 
+obs_GFED = obs_global[['GFED5']]
+obs_DF = (obs_GFED-obs_GFED.mean(axis=0))/obs_GFED.mean(axis=0)
+
+GFED5weights = []
+for modelname in model_DF.columns:    
+    Weights = log_diff(obs_DF.values, model_DF[modelname].values)
+    GFED5weights.append(Weights)
+print(GFED5weights)
+
+# Make temporal NME for FireCCI 
+obs = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_obs_df.pkl')#2 obs, CCI5.1 & MCD64
+obs_time = df_constrain_time(obs, 2003, 2019)
+obs_global = obs_time.groupby(by='Observation', axis=1).sum(min_count=1) 
+obs_CCI = obs_global[['FireCCI5.1']]
+obs_DF = (obs_CCI-obs_CCI.mean(axis=0))/obs_CCI.mean(axis=0)
+
+FireCCIweights = []
+for modelname in model_DF.columns:    
+    Weights = log_diff(obs_DF.values, model_DF[modelname].values)
+    FireCCIweights.append(Weights)
+print(FireCCIweights)
+
+#"Broadcast" the results for GFED into a big dataframe to get the right size later (204 rows)
+GFED5weights_df = pd.DataFrame(columns=obsclim.columns.unique(level='Model'))
+for n in np.arange(204):
+    GFED5weights_df.loc[len(GFED5weights_df)] = GFED5weights
+
+OBSweights_df = (GFED5weights_df + FireCCIweights_df) / 2
+print(OBSweights_df)
 
 
-# Make FireCCI NME
-obs_CCI = (obs_constrainedtime['FireCCI5.1'])
-obs_DF_CCI = (obs_CCI-obs_CCI.mean(axis=0))/obs_CCI.mean(axis=0)
-CCIweights = (np.abs(np.log(model_DF+1.0+(1/204))).subtract(np.abs(np.log(obs_DF_CCI+1.0+(1/204))),axis=0))# Model vs Obs anomaly
-CCIweights[:] = CCIweights.mean(axis=0)
 
 
-OBSweights = (GFED5weights + CCIweights)/2
-print(OBSweights)
-
-
-###Add uncertainty####
+################################ Get Data ###################################
 
 import math
 obsclim = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_obsclim_df.pkl')
@@ -193,50 +92,59 @@ obsclim = df_constrain_time(obsclim_global, 2003, 2019)
 counterclim = df_constrain_time(counterclim_global, 2003, 2019)
 
 
+################################ Add Uncertainty ###################################
 
-#######
-
-def run_RR(obsclim, counterclim, add_noise = True):
-   
-    def add_random_noise(series):
-        Noise = series.copy()
-        for modelname in Noise.columns.unique(level='Model'):
-            Noise[modelname]= (np.random.normal(loc=0, scale=np.sqrt(math.pi/2), size=len(series)))
-        series = np.log(series) + Noise * OBSweights
-        series = np.exp(series)
-        return(series)
+def run_RR(add_noise = True):
+    rng = np.random.default_rng(SEED)
     
-    #Calculate the uncertainty by adding noise into distribution
-    if add_noise:
-        obsclim = add_random_noise(obsclim) 
-        counterclim = add_random_noise(counterclim) 
-
-    #Calculate Relative anomaly and resample
-    rng = np.random.default_rng(SEED)
-    df = (obsclim-counterclim.mean(axis=0))/counterclim.mean(axis=0)
-    series = pd.Series(df.values.ravel())
-    obsclim_sampled_global = series.sample(n=100000, weights=np.tile(select_region(model_weights, 'Global').values.ravel(), len(df.index)), random_state=rng, replace=True)
-
-    rng = np.random.default_rng(SEED)
-    df = (counterclim-counterclim.mean(axis=0))/counterclim.mean(axis=0)
-    series = pd.Series(df.values.ravel())
-    counterclim_sampled_global = series.sample(n=100000, weights=np.tile(select_region(model_weights, 'Global').values.ravel(), len(df.index)), random_state=rng, replace=True)
+    def add_random_noise(model, noise):
+    
+        # Your logic here
+        random_numbers = np.random.normal(loc=0, scale=np.sqrt(math.pi/2), size=len(model)) # Get the random numbers
+        log_model = np.log(model)
+        series = log_model + random_numbers * noise.values
+        series = series - np.mean(series)
+        series = series * np.std(log_model)/np.std(series)
+        series = series + np.mean(log_model)
+        series = np.exp(series)
+        return series  # Example: Adding the two columns
+    
+    def add_noise_to_experiment(models):
+        if add_noise: models = models.apply(lambda col1: add_random_noise(col1, OBSweights_df[col1.name]), axis=0)
+        
+        return pd.Series(models.values.ravel())
+    
+    #Calculate the uncertainty for obsclim
+    obsclim_series = add_noise_to_experiment(obsclim)
+    counterclim_series = add_noise_to_experiment(counterclim) 
+    counterclim_mean = counterclim_series.mean(axis=0)
+    
+    weights = np.tile(select_region(model_weights, 'Global').values.ravel(), 204)
+    weights = weights / np.sum(weights)
+    
+    def randomly_sample_RA(experiment):
+        RA = (experiment-counterclim_mean)/counterclim_mean
+        return np.random.choice(RA, 10000, p = weights, replace=True)
+    
+    obsclim_sampled_global = randomly_sample_RA(obsclim_series)
+    counterclim_sampled_global = randomly_sample_RA(counterclim_series)
     
     ##Calculate the Probability Ratio
     ALL = (np.count_nonzero(obsclim_sampled_global > (counterclim_sampled_global.mean(axis=0))))
     NAT = (np.count_nonzero(counterclim_sampled_global > (counterclim_sampled_global.mean(axis=0))))
     PR = ALL/NAT
     
-    burn_change = obsclim_sampled_global.mean(axis=0)*100 - counterclim_sampled_global.mean(axis=0)*100
+    burn_change = np.mean(obsclim_sampled_global)
+    
     return PR, burn_change
 
 
-PR_noNoise, change_noNoise = run_RR(obsclim, counterclim, False)
-PRuncertEnsemble = np.array([run_RR(obsclim, counterclim, True) for i in range(1000)])
+PR_noNoise, change_noNoise  = run_RR(False)
+PRuncertEnsemble = np.array([run_RR(True) for i in range(1000)])
 
+################################ Print results ###################################
 
-
-#####Get the 10th and 90th percentile for the new uncertainty ensemble
+##Get the 10th and 90th percentile for the new uncertainty ensemble
 PR_uncertainty_range = np.percentile(PRuncertEnsemble[:,0], [10, 90])
 change_uncertainty_range = np.percentile(PRuncertEnsemble[:,1], [10, 90])
 
@@ -248,16 +156,20 @@ print_rounded_result("    range:", PR_uncertainty_range)
 print_rounded_result("    p-value:", np.mean(PRuncertEnsemble[:,0] < 1.0))
 
 
-print_rounded_result("change in burnt area:", change_noNoise)
+print_rounded_result("change on burnt area:", change_noNoise)
 print_rounded_result("    range:", change_uncertainty_range)
 print_rounded_result("    p-value:", np.mean(PRuncertEnsemble[:,1] < 0.0))
 
 
+
+'''
 PR: 1.43
-    range:[1.36 1.42]
+    range:[1.4  1.48]
     p-value:0.0
-change in burnt area:16.43
-    range:[15.66 17.09]
+change on burnt area:0.16
+    range:[0.15 0.16]
     p-value:0.0
+
+'''
 
 
--- CodeForPlots/Regional_Uncertainty_Analysis.py
+++ CodeForPlots/Regional_Uncertainty_Analysis.py
@@ -1,5 +1,31 @@
+import os
+import numpy as np
+import seaborn as sns
+import cf_units
+import cftime
+import cartopy
+import cartopy.crs as ccrs
+import geopandas as gpd
+import warnings
+import pandas as pd
+import pickle
+from copy import deepcopy
+from scipy import stats
+from scipy.signal import periodogram
+from scipy.stats import pearsonr
+import random
+warnings.filterwarnings('ignore')
 
-### FINAL Working regional code ####
+
+def df_constrain_time(df, start, end):
+    return df.loc[str(start):str(end)]
+def select_region(df, regionname):
+    return df.loc[:, (regionname, slice(None))]
+def select_model(df, modelname):
+    return df.loc[:, (slice(None), modelname)]
+SEED = int.from_bytes("They're taking the hobbits to Isengard!".encode('utf-8'), byteorder='big')
+
+
 
 
 #######################################Get weights########################################################
@@ -14,8 +40,6 @@ model_DF = (obsclim-obsclim.mean(axis=0))/obsclim.mean(axis=0)
 
 ##Set up obs relative anomaly
 
-def select_obs(df, modelname):
-    return df.loc[:, (slice(None), modelname)]
     
 # Make GFED5 NME
 obs = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_obs_df.pkl')
@@ -38,8 +62,7 @@ FireCCIweights[:] = FireCCIweights.mean(axis=0)
 
 #Average both obs datasets
 OBSweights = (GFED5weights + FireCCIweights)/2
-
-print(OBSweights)
+#print(OBSweights)
 
 
 ####################################################### Load data######################################################
@@ -54,27 +77,23 @@ counterclim = df_constrain_time(counterclim, 2003, 2019)
 
 ### Regional Uncertainty results
 
-import math
-obsclim = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_obsclim_df.pkl')
-counterclim = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_counterclim_df.pkl')
-
-obsclim = df_constrain_time(obsclim, 2003, 2019)
-counterclim = df_constrain_time(counterclim, 2003, 2019)
-
 #######
 def run_RR(obsclim, counterclim, add_noise = True):
-
     
     def add_random_noise(series):
         Noise = series.copy()
         for regionname in series.columns.unique(level='Region'):                 
             for modelname in series.columns.unique(level='Model'):
                  Noise[regionname,modelname]= (np.random.normal(loc=0, scale=np.sqrt(math.pi/2), size=len(series)))
-        series = np.log(series) + Noise * OBSweights
+        log_model = np.log(series+(1/len(series)))
+        series = log_model + Noise * OBSweights
+        series = series - np.mean(series, axis=0)
+        series = series * np.std(log_model)/np.std(series)
+        series = series + np.mean(log_model)
         series = np.exp(series)
         return(series)
     
-    #Add noise second (calculate the uncertainty by adding noise into distribution)
+    #Add noise second if TRUE (calculate the uncertainty by adding noise into distribution)
     if add_noise:
         obsclim = add_random_noise(obsclim) 
         counterclim = add_random_noise(counterclim) 
@@ -106,6 +125,7 @@ def run_RR(obsclim, counterclim, add_noise = True):
 
     burn_change = obsclim_sampled_regional.mean(axis=0)*100 - counterclim_sampled_regional.mean(axis=0)*100
 
+
     PRlist = []
     for regionname in df.columns.unique(level='Region'):
         ALL = (np.count_nonzero(obsclim_sampled_regional.loc[slice(None), regionname] > (counterclim_sampled_regional.loc[slice(None), regionname].mean(axis=0))))
@@ -118,19 +138,18 @@ def run_RR(obsclim, counterclim, add_noise = True):
 
     
     if add_noise == False:
-        Table = pd.DataFrame(burn_change, columns=['Burn Change'])
+        Table = pd.DataFrame(burn_change, columns=['BurnChange'])
         Table['PR'] = PRlist
         return Table
-    #return PR, burn_change
+
 
 ### FIRST read in the data###
-PR_noNoise, change_noNoise = run_RR(obsclim, counterclim, False)
 Table = (run_RR(obsclim, counterclim, False))
 
-Burn, PR = zip(*[run_RR(obsclim, counterclim, True) for i in range(10)])
+Burn, PR = zip(*[run_RR(obsclim, counterclim, True) for i in range(1000)])
 
-PRUncertTable = pd.DataFrame(PR, columns=df.columns.unique(level='Region'))
-BurnUncertTable = pd.DataFrame(Burn, columns=df.columns.unique(level='Region'))
+PRUncertTable = pd.DataFrame(PR, columns=obsclim.columns.unique(level='Region'))
+BurnUncertTable = pd.DataFrame(Burn, columns=obsclim.columns.unique(level='Region'))
 
 Table["Burn10"] = (BurnUncertTable[:].quantile(0.1))
 Table["Burn90"] = (BurnUncertTable[:].quantile(0.9))
@@ -138,7 +157,8 @@ Table["PR10"] = (PRUncertTable[:].quantile(0.1))
 Table["PR90"] = (PRUncertTable[:].quantile(0.9))
 Table = Table.round(decimals=2)
 
-Table = Table[['Burn Change', 'Burn10', 'Burn90', 'PR', 'PR10', 'PR90']]
+Table = Table[['BurnChange', 'Burn10', 'Burn90', 'PR', 'PR10', 'PR90']]
 print(Table.astype(float).round(2))
 
-
+Table.to_pickle("~/GitHub/ISIMIP3a/Supplementary_Data.Uncertainty.pkl") 
+print('Saved')
