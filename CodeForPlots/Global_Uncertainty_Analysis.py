1eb3fba3f74bc6de1308db8b77373c8710efcf1f
chantelle.burton <chantelle.burton@.metoffice.gov.uk>
2023-11-22 09:52:35 +0000

    Tidying up the random noise, rearrange after adding noise to sigmaD

--- CodeForPlots/Global_Uncertainty_Analysis.py
+++ CodeForPlots/Global_Uncertainty_Analysis.py
@@ -26,8 +26,8 @@ SEED = int.from_bytes("They're taking the hobbits to Isengard!".encode('utf-8'),
 
 obsclim_df = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_obsclim_df.pkl')
 counterclim_df = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_counterclim_df.pkl')
-model_weights = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/NME3_Weights.pkl')
-
+#model_weights = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/NME3_Weights.pkl')
+model_weights = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Weights_optimal_sigmaD.pkl')
 
 
 #######################################Get weights########################################################
@@ -38,8 +38,8 @@ obsclim_global = obsclim_df.groupby('Model', axis=1).sum()
 obsclim = df_constrain_time(obsclim_global, 2003, 2019)
 model_DF = (obsclim-obsclim.mean(axis=0))/obsclim.mean(axis=0)
 
-#Set up obs relative anomaly
 
+#Set up obs relative anomaly
 # Make temporal NME for GFED5 first
 obs = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_obs_df.pkl')#2 obs, CCI5.1 & MCD64
 obs_time = df_constrain_time(obs, 2003, 2019)
@@ -47,12 +47,13 @@ obs_global = obs_time.groupby(by='Observation', axis=1).sum(min_count=1)
 obs_GFED = obs_global[['GFED5']]
 obs_DF = (obs_GFED-obs_GFED.mean(axis=0))/obs_GFED.mean(axis=0)
 
-
-GFED5weights = model_DF.copy()
+GFED5_NME = model_DF.copy()
 for modelname in model_DF.columns:
-    GFED5weights[modelname] = np.log(GFED5weights[modelname]+1.0+(1/204)).subtract(np.log(obs_DF['GFED5']+1.0+(1/204)), axis=0)
-GFED5weights = np.abs(GFED5weights)
-GFED5weights[:] = GFED5weights.mean(axis=0)
+    GFED5_NME[modelname] = GFED5_NME[modelname].subtract(obs_DF['GFED5'], axis=0)
+GFED5_NME = np.abs(GFED5_NME)
+GFED5_NME[:] = GFED5_NME.mean(axis=0)
+
+
 
 
 # Make temporal NME for FireCCI 
@@ -62,15 +63,14 @@ obs_global = obs_time.groupby(by='Observation', axis=1).sum(min_count=1)
 obs_CCI = obs_global[['FireCCI5.1']]
 obs_DF = (obs_CCI-obs_CCI.mean(axis=0))/obs_CCI.mean(axis=0)
 
-
-CCIweights = model_DF.copy()
+CCI_NME = model_DF.copy()
 for modelname in model_DF.columns:
-    CCIweights[modelname] = np.log(CCIweights[modelname]+1.0+(1/204)).subtract(np.log(obs_DF['FireCCI5.1']+1.0+(1/204)), axis=0)
-CCIweights = np.abs(CCIweights)
-CCIweights[:] = CCIweights.mean(axis=0)
+    CCI_NME[modelname] = CCI_NME[modelname].subtract(obs_DF['FireCCI5.1'], axis=0)
+CCI_NME = np.abs(CCI_NME)
+CCI_NME[:] = CCI_NME.mean(axis=0)
 
 
-OBSweights = (GFED5weights + CCIweights)/2
+NME = (GFED5_NME + CCI_NME)/2
 
 
 ################################ Get Data ###################################
@@ -81,115 +81,115 @@ counterclim = pd.read_pickle(f'/scratch/cburton/scratch/ISIMIP3a/Data/AR6_counte
 
 obsclim_global = obsclim_df.groupby('Model', axis=1).sum()
 counterclim_global = counterclim_df.groupby('Model', axis=1).sum()
-###Change these for PD / DHF / ALL ####
+
+model_weights = model_weights.reindex(sorted(model_weights.columns), axis = 1)
+NME = NME.reindex(sorted(NME.columns), axis = 1)
+
+def add_uncertainty(obsclim, counterclim):
+   
+    obsclim = obsclim.reindex(sorted(obsclim.columns), axis = 1)
+    counterclim = counterclim.reindex(sorted(counterclim.columns), axis = 1)
+
+################################ Add Uncertainty ###################################
+
+    def run_RR(add_noise = True, i=0):
+        rng = np.random.default_rng(SEED)
+    
+        #(2b) add the random noise
+        def add_random_noise(model, NME):
+            Noise = model.copy()
+            for modelname in model.columns.unique(level='Model'):
+                Noise[modelname] = np.random.normal(loc=0, scale=np.sqrt(math.pi/2), size=204) # Get the random numbers
+            model = model + Noise * NME.to_numpy()
+            return model  
+    
+        #(2a) add noise if TRUE
+        def add_noise_to_experiment(model):
+            if add_noise: model = add_random_noise(model, NME)
+            return (model)
+
+        #(4a)### find RA after adding in the noise
+        def randomly_sample_RA(experiment):
+            RA = (experiment-counterclim_mean)/counterclim_mean
+            RA = pd.Series(RA.values.ravel())
+            return np.random.choice(RA, 100000, p = weights, replace=True)    
+
+        #(2) Call function add_noise
+        obsclim_series = add_noise_to_experiment(obsclim)
+        counterclim_series = add_noise_to_experiment(counterclim) 
+        counterclim_mean = counterclim_series.mean(axis=0)
+
+        #(3) get the weights ready to use
+        weights = np.tile(select_region(model_weights, 'Global').values.ravel(), 204)
+        weights = weights / np.sum(weights)
+
+        #(4) make the bootstrapped, resampled relative anomaly
+        obsclim_sampled_global = randomly_sample_RA(obsclim_series)
+        counterclim_sampled_global = randomly_sample_RA(counterclim_series)
+    
+        #(5)
+        ##Calculate the Probability Ratio
+        ALL = (np.count_nonzero(obsclim_sampled_global > (counterclim_sampled_global.mean(axis=0))))
+        NAT = (np.count_nonzero(counterclim_sampled_global > (counterclim_sampled_global.mean(axis=0))))
+        PR = ALL/NAT
+    
+        burn_change = np.mean(obsclim_sampled_global)
+    
+        return PR, burn_change
+
+    #(1) Call the functions
+    PR_noNoise, change_noNoise  = run_RR(False)
+    PRuncertEnsemble = np.array([run_RR(True) for i in range(1000)])
+
+    ################################ Print results ###################################
+
+    ##Get the 10th and 90th percentile for the new uncertainty ensemble
+    PR_uncertainty_range = np.percentile(PRuncertEnsemble[:,0], [10, 90])
+    change_uncertainty_range = np.percentile(PRuncertEnsemble[:,1], [10, 90])
+
+    CHG = (np.percentile(PRuncertEnsemble[:,0], 50))
+    RNG = (PR_uncertainty_range[1]) - CHG
+    print('PR:',f'{CHG.round(2)} ± {RNG.round(2)}')
+
+    CHG = np.percentile(PRuncertEnsemble[:,1], 50)*100
+    RNG = (change_uncertainty_range[1]*100) - CHG
+    print('BA chg:',f'{CHG.round(2)} ± {RNG.round(2)}')
+
+
+
+###All exps PD / DHF / ALL ####
 #PD
-#print('PD')
-#obsclim = df_constrain_time(obsclim_global, 2003, 2019)
-#counterclim = df_constrain_time(counterclim_global, 2003, 2019)
+print('PD')
+obsclim = df_constrain_time(obsclim_global, 2003, 2019)
+counterclim = df_constrain_time(counterclim_global, 2003, 2019)
+add_uncertainty(obsclim, counterclim)
 #DHF
-#print('DHF')
-#obsclim = df_constrain_time(counterclim_global, 2003, 2019)
-#counterclim = df_constrain_time(counterclim_global, 1901, 1917)
+print('DHF')
+obsclim = df_constrain_time(counterclim_global, 2003, 2019)
+counterclim = df_constrain_time(counterclim_global, 1901, 1917)
+add_uncertainty(obsclim, counterclim)
 #All
 print('ALL')
 obsclim = df_constrain_time(obsclim_global, 2003, 2019)
 counterclim = df_constrain_time(counterclim_global, 1901, 1917)
+add_uncertainty(obsclim, counterclim)
 
 
-obsclim = obsclim.reindex(sorted(obsclim.columns), axis = 1)
-counterclim = counterclim.reindex(sorted(counterclim.columns), axis = 1)
-model_weights = model_weights.reindex(sorted(model_weights.columns), axis = 1)
-OBSweights = OBSweights.reindex(sorted(OBSweights.columns), axis = 1)
-
-################################ Add Uncertainty ###################################
-
-def run_RR(add_noise = True, i=0):
-    rng = np.random.default_rng(SEED)
-    
-    #(2b)
-    def add_random_noise(model, OBSweights):
-
-
-        Noise = model.copy()
-        for modelname in model.columns.unique(level='Model'):
-            Noise[modelname] = np.random.normal(loc=0, scale=np.sqrt(math.pi/2), size=204) # Get the random numbers
-        log_model = np.log(model+(1/len(model)))
-        series = log_model + Noise * OBSweights.to_numpy()
-        series = series - np.mean(series, axis=0)
-        series = series / np.std(series, axis=0)
-        series = series * np.std(log_model)/np.std(series)
-        series = series + np.mean(log_model)
-        series = np.exp(series)
-        return series  
-    
-    #(2a)
-    def add_noise_to_experiment(models):
-        if add_noise: models = add_random_noise(models, OBSweights)
-        return (models)
-    
-    #(2)
-    obsclim_series = add_noise_to_experiment(obsclim)
-    counterclim_series = add_noise_to_experiment(counterclim) 
-    counterclim_mean = counterclim_series.mean(axis=0)
-    #(3)
-    weights = np.tile(select_region(model_weights, 'Global').values.ravel(), 204)
-    weights = weights / np.sum(weights)
-    
-    #(4a)
-    def randomly_sample_RA(experiment):
-        RA = (experiment-counterclim_mean)/counterclim_mean
-        RA = pd.Series(RA.values.ravel())
-        return np.random.choice(RA, 100000, p = weights, replace=True)
-    #(4)
-    obsclim_sampled_global = randomly_sample_RA(obsclim_series)
-    counterclim_sampled_global = randomly_sample_RA(counterclim_series)
-    
-    #(5)
-    ##Calculate the Probability Ratio
-    ALL = (np.count_nonzero(obsclim_sampled_global > (counterclim_sampled_global.mean(axis=0))))
-    NAT = (np.count_nonzero(counterclim_sampled_global > (counterclim_sampled_global.mean(axis=0))))
-    PR = ALL/NAT
-    
-    burn_change = np.mean(obsclim_sampled_global)
-    
-    return PR, burn_change
-
-#(1)
-PR_noNoise, change_noNoise  = run_RR(False)
-PRuncertEnsemble = np.array([run_RR(True) for i in range(1000)])
-
-################################ Print results ###################################
-
-##Get the 10th and 90th percentile for the new uncertainty ensemble
-PR_uncertainty_range = np.percentile(PRuncertEnsemble[:,0], [10, 90])
-change_uncertainty_range = np.percentile(PRuncertEnsemble[:,1], [10, 90])
-
-CHG = (np.mean(PR_uncertainty_range))
-RNG = (PR_uncertainty_range[1]) - CHG
-print(f'{CHG.round(2)} ± {RNG.round(2)}')
-
-CHG = np.mean(change_uncertainty_range)*100
-RNG = (change_uncertainty_range[1]*100) - CHG
-print(f'{CHG.round(2)} ± {RNG.round(2)}')
 
 
 
 '''
-#PD
-PR: 1.43
-    range:[1.39  1.47]
-    p-value:0.0
-change in burnt area:0.16
-    range:[0.16 0.17]
-    p-value:0.0
+#Results
 
-#DHF
-0.46 ± 0.02
--15.67 ± 0.15
-
-#All
-0.82 ± 0.03
--3.54 ± 0.15
+PD
+1.5 ± 0.06
+21.97 ± 0.2
+DHF
+0.34 ± 0.03
+-22.18 ± 0.11
+ALL
+0.78 ± 0.04
+-6.32 ± 0.13
 
 
 
